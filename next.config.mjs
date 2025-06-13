78848dc913289b555d4abbca6484ef45a232f89f
finger <finger.xie@foxmail.com>
2025-06-14 02:41:51 +0800

    feat 通过 catch，将所有的请求都托管至首页

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return {
+      fallback: [
+        {
+          source: '/((?!_next|api|favicon.ico).*)',
+          destination: '/',
+        },
+      ],
+    };
+  },
+};
 
 export default nextConfig;
